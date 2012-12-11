8a9a2a860772ec71c517c4ae4dd93aa4ab2ea342
JD Isaacks <jd@jisaacks.com>
2012-12-10 20:55:43 -0500

    Add settings to turn off live mode

--- git_gutter_events.py
+++ git_gutter_events.py
@@ -3,14 +3,16 @@ import sublime_plugin
 import view_collection
 
 class GitGutterEvents(sublime_plugin.EventListener):
-  def on_new(self, view):
-    view_collection.ViewCollection.add(view)
 
   def on_load(self, view):
     view_collection.ViewCollection.add(view)
 
   def on_modified(self, view):
-    view_collection.ViewCollection.add(view)
+    if view.settings().get('git_gutter_live_mode', True):
+      view_collection.ViewCollection.add(view)
 
   def on_clone(self, view):
+    view_collection.ViewCollection.add(view)
+
+  def on_post_save(self, view):
     view_collection.ViewCollection.add(view)
