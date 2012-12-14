3659c73dc184ce8f8e4056d06237b9a8f88cd758
Michael Fogleman <fogleman@gmail.com>
2012-12-13 19:52:22 -0500

    PEP-8 Formatting: 4 space tabs

--- git_gutter.py
+++ git_gutter.py
@@ -3,57 +3,57 @@ import sublime_plugin
 from view_collection import ViewCollection
 
 class GitGutterCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    self.clear_all()
+    def run(self, edit):
+        self.clear_all()
 
-    (inserted, modified, deleted) = ViewCollection.diff(self.view)
+        (inserted, modified, deleted) = ViewCollection.diff(self.view)
 
-    self.lines_removed(deleted)
-    self.lines_added(inserted)
-    self.lines_modified(modified)
+        self.lines_removed(deleted)
+        self.lines_added(inserted)
+        self.lines_modified(modified)
 
-  def clear_all(self):
-    self.view.erase_regions('git_gutter_deleted_top')
-    self.view.erase_regions('git_gutter_deleted_bottom')
-    self.view.erase_regions('git_gutter_inserted')
-    self.view.erase_regions('git_gutter_changed')
+    def clear_all(self):
+        self.view.erase_regions('git_gutter_deleted_top')
+        self.view.erase_regions('git_gutter_deleted_bottom')
+        self.view.erase_regions('git_gutter_inserted')
+        self.view.erase_regions('git_gutter_changed')
 
-  def lines_to_regions(self, lines):
-    regions = []
-    for line in lines:
-      position = self.view.text_point(line-1, 0)
-      region   = sublime.Region(position,position)
-      regions.append(region)
-    return regions
+    def lines_to_regions(self, lines):
+        regions = []
+        for line in lines:
+            position = self.view.text_point(line-1, 0)
+            region = sublime.Region(position,position)
+            regions.append(region)
+        return regions
 
-  def lines_removed(self, lines):
-    bottom_lines = []
-    for line in lines:
-      if not line == 1:
-        bottom_lines.append(line-1)
-    self.lines_removed_top(lines)
-    self.lines_removed_bottom(bottom_lines)
+    def lines_removed(self, lines):
+        bottom_lines = []
+        for line in lines:
+            if not line == 1:
+                bottom_lines.append(line-1)
+        self.lines_removed_top(lines)
+        self.lines_removed_bottom(bottom_lines)
 
-  def lines_removed_top(self, lines):
-    regions = self.lines_to_regions(lines)
-    scope   = "markup.deleted"
-    icon    = '../GitGutter/icons/deleted_top'
-    self.view.add_regions('git_gutter_deleted_top', regions, scope, icon)
+    def lines_removed_top(self, lines):
+        regions = self.lines_to_regions(lines)
+        scope = 'markup.deleted'
+        icon = '../GitGutter/icons/deleted_top'
+        self.view.add_regions('git_gutter_deleted_top', regions, scope, icon)
 
-  def lines_removed_bottom(self, lines):
-    regions = self.lines_to_regions(lines)
-    scope   = "markup.deleted"
-    icon    = '../GitGutter/icons/deleted_bottom'
-    self.view.add_regions('git_gutter_deleted_bottom', regions, scope, icon)
+    def lines_removed_bottom(self, lines):
+        regions = self.lines_to_regions(lines)
+        scope = 'markup.deleted'
+        icon = '../GitGutter/icons/deleted_bottom'
+        self.view.add_regions('git_gutter_deleted_bottom', regions, scope, icon)
 
-  def lines_added(self, lines):
-    regions = self.lines_to_regions(lines)
-    scope   = "markup.inserted"
-    icon    = '../GitGutter/icons/inserted'
-    self.view.add_regions('git_gutter_inserted', regions, scope, icon)
+    def lines_added(self, lines):
+        regions = self.lines_to_regions(lines)
+        scope = 'markup.inserted'
+        icon = '../GitGutter/icons/inserted'
+        self.view.add_regions('git_gutter_inserted', regions, scope, icon)
 
-  def lines_modified(self, lines):
-    regions = self.lines_to_regions(lines)
-    scope   = "markup.changed"
-    icon    = '../GitGutter/icons/changed'
-    self.view.add_regions('git_gutter_changed', regions, scope, icon)
+    def lines_modified(self, lines):
+        regions = self.lines_to_regions(lines)
+        scope = 'markup.changed'
+        icon = '../GitGutter/icons/changed'
+        self.view.add_regions('git_gutter_changed', regions, scope, icon)
--- view_collection.py
+++ view_collection.py
@@ -2,57 +2,57 @@ import tempfile
 import time
 
 class ViewCollection:
-  views = {}
-  git_times = {}
-  git_files = {}
-  buf_files = {}
-  
-  @staticmethod
-  def add(view):
-    key = ViewCollection.get_key(view)
-    from git_gutter_handler import GitGutterHandler
-    ViewCollection.views[key] = GitGutterHandler(view)
-    ViewCollection.views[key].reset()
+    views = {}
+    git_times = {}
+    git_files = {}
+    buf_files = {}
+    
+    @staticmethod
+    def add(view):
+        key = ViewCollection.get_key(view)
+        from git_gutter_handler import GitGutterHandler
+        ViewCollection.views[key] = GitGutterHandler(view)
+        ViewCollection.views[key].reset()
 
-  @staticmethod
-  def git_path(view):
-    key = ViewCollection.get_key(view)
-    if key in ViewCollection.views:
-      return ViewCollection.views[key].get_git_path()
-    else:
-      return False
+    @staticmethod
+    def git_path(view):
+        key = ViewCollection.get_key(view)
+        if key in ViewCollection.views:
+            return ViewCollection.views[key].get_git_path()
+        else:
+            return False
 
-  @staticmethod
-  def get_key(view): 
-    return view.file_name()
+    @staticmethod
+    def get_key(view): 
+        return view.file_name()
 
-  @staticmethod
-  def diff(view):
-    key = ViewCollection.get_key(view)
-    return ViewCollection.views[key].diff()
+    @staticmethod
+    def diff(view):
+        key = ViewCollection.get_key(view)
+        return ViewCollection.views[key].diff()
 
-  @staticmethod
-  def git_time(view):
-    key = ViewCollection.get_key(view)
-    if not key in ViewCollection.git_times:
-      ViewCollection.git_times[key] = 0
-    return time.time() - ViewCollection.git_times[key]
+    @staticmethod
+    def git_time(view):
+        key = ViewCollection.get_key(view)
+        if not key in ViewCollection.git_times:
+            ViewCollection.git_times[key] = 0
+        return time.time() - ViewCollection.git_times[key]
 
-  @staticmethod
-  def update_git_time(view):
-    key = ViewCollection.get_key(view)
-    ViewCollection.git_times[key] = time.time()
+    @staticmethod
+    def update_git_time(view):
+        key = ViewCollection.get_key(view)
+        ViewCollection.git_times[key] = time.time()
 
-  @staticmethod
-  def git_tmp_file(view):
-    key = ViewCollection.get_key(view)
-    if not key in ViewCollection.git_files:
-      ViewCollection.git_files[key] = tempfile.NamedTemporaryFile()
-    return ViewCollection.git_files[key]
+    @staticmethod
+    def git_tmp_file(view):
+        key = ViewCollection.get_key(view)
+        if not key in ViewCollection.git_files:
+            ViewCollection.git_files[key] = tempfile.NamedTemporaryFile()
+        return ViewCollection.git_files[key]
 
-  @staticmethod 
-  def buf_tmp_file(view):
-    key = ViewCollection.get_key(view)
-    if not key in ViewCollection.buf_files:
-      ViewCollection.buf_files[key] = tempfile.NamedTemporaryFile()
-    return ViewCollection.buf_files[key]
+    @staticmethod 
+    def buf_tmp_file(view):
+        key = ViewCollection.get_key(view)
+        if not key in ViewCollection.buf_files:
+            ViewCollection.buf_files[key] = tempfile.NamedTemporaryFile()
+        return ViewCollection.buf_files[key]
