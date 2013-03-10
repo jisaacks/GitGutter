cc9ea4bbb69e2cab6e7cf373408d12854a262e23
JD Isaacks <jd@jisaacks.com>
2013-03-10 10:59:19 -0400

    Add non blocking mode to run gitgutter in seperate thread

--- git_gutter_events.py
+++ git_gutter_events.py
@@ -9,21 +9,56 @@ class GitGutterEvents(sublime_plugin.EventListener):
     def __init__(self):
         self.load_settings()
 
+    # Synchronous
+
     def on_modified(self, view):
-        if self.live_mode:
+        if not self.live_mode:
+            return None
+        if not self.non_blocking:
             ViewCollection.add(view)
 
     def on_clone(self, view):
-        ViewCollection.add(view)
+        if not self.non_blocking:
+            ViewCollection.add(view)
 
     def on_post_save(self, view):
-        ViewCollection.add(view)
+        if not self.non_blocking:
+            ViewCollection.add(view)
 
     def on_activated(self, view):
-        ViewCollection.add(view)
+        if not self.non_blocking:
+            ViewCollection.add(view)
+
+    
+    # Asynchronous
+
+    def on_modified_async(self, view):
+        if not self.live_mode:
+            return None
+        if self.non_blocking:
+            ViewCollection.add(view)
+
+    def on_clone_async(self, view):
+        if self.non_blocking:
+            ViewCollection.add(view)
+
+    def on_post_save_async(self, view):
+        if self.non_blocking:
+            ViewCollection.add(view)
+
+    def on_activated_async(self, view):
+        if self.non_blocking:
+            ViewCollection.add(view)
+
+    # Settings
 
     def load_settings(self):
         self.settings = sublime.load_settings('GitGutter.sublime-settings')
+        
         self.live_mode = self.settings.get('live_mode')
         if self.live_mode is None: 
             self.live_mode = True
+
+        self.non_blocking = self.settings.get('non_blocking')
+        if self.non_blocking is None or int(sublime.version()) < 3014: 
+            self.non_blocking = False
