6b54e38523bd19a893e42b56410e54a8e833b6f9
JD Isaacks <jd@jisaacks.com>
2013-10-28 21:49:30 -0400

    Don't load settings until plugin_loaded

--- git_gutter_events.py
+++ git_gutter_events.py
@@ -1,76 +1,100 @@
 import sublime
 import sublime_plugin
-try:
+
+ST3 = int(sublime.version()) >= 3000
+if ST3:
     from GitGutter.view_collection import ViewCollection
-except ImportError:
+else:
     from view_collection import ViewCollection
 
 
 class GitGutterEvents(sublime_plugin.EventListener):
 
     def __init__(self):
-        self.load_settings()
+        self._settings_loaded = False
 
     # Synchronous
 
     def on_modified(self, view):
-        if not self.live_mode:
-            return None
-        if not self.non_blocking:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if not self.non_blocking and self.live_mode:
+                ViewCollection.add(view)
 
     def on_clone(self, view):
-        if not self.non_blocking:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if not self.non_blocking:
+                ViewCollection.add(view)
 
     def on_post_save(self, view):
-        if not self.non_blocking:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if not self.non_blocking:
+                ViewCollection.add(view)
 
     def on_load(self, view):
-        if not self.non_blocking and not self.live_mode:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if not self.non_blocking and not self.live_mode:
+                ViewCollection.add(view)
 
     def on_activated(self, view):
-        if not self.non_blocking and self.focus_change_mode:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if not self.non_blocking and self.focus_change_mode:
+                ViewCollection.add(view)
 
     # Asynchronous
+
     def on_modified_async(self, view):
-        if not self.live_mode:
-            return None
-        if self.non_blocking:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if self.non_blocking and self.live_mode:
+                ViewCollection.add(view)
 
     def on_clone_async(self, view):
-        if self.non_blocking:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if self.non_blocking:
+                ViewCollection.add(view)
 
     def on_post_save_async(self, view):
-        if self.non_blocking:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if self.non_blocking:
+                ViewCollection.add(view)
 
     def on_load_async(self, view):
-        if self.non_blocking and not self.live_mode:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if self.non_blocking and not self.live_mode:
+                ViewCollection.add(view)
 
     def on_activated_async(self, view):
-        if self.non_blocking and self.focus_change_mode:
-            ViewCollection.add(view)
+        if self.settings_loaded():
+            if self.non_blocking and self.focus_change_mode:
+                ViewCollection.add(view)
 
     # Settings
 
-    def load_settings(self):
-        self.settings = sublime.load_settings('GitGutter.sublime-settings')
+    def settings_loaded(self):
+        if settings and not self._settings_loaded:
+            self._settings_loaded = self.load_settings()
 
-        self.live_mode = self.settings.get('live_mode')
+        return self._settings_loaded
+
+
+    def load_settings(self):
+        self.live_mode = settings.get('live_mode')
         if self.live_mode is None:
             self.live_mode = True
 
-        self.focus_change_mode = self.settings.get('focus_change_mode')
+        self.focus_change_mode = settings.get('focus_change_mode')
         if self.focus_change_mode is None:
             self.focus_change_mode = True
 
-        self.non_blocking = self.settings.get('non_blocking')
+        self.non_blocking = settings.get('non_blocking')
         if self.non_blocking is None or int(sublime.version()) < 3014:
             self.non_blocking = False
+
+        return True
+
+def plugin_loaded():
+    global settings
+    settings = sublime.load_settings('GitGutter.sublime-settings')
+
+if not ST3:
+    plugin_loaded()
+
