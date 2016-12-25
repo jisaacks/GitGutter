71e43a9a53c61d7177e8bbd2bd73910d87eab0cd
DeathAxe <deathaxe@web.de>
2016-12-26 00:49:38 +0100

    Feature: Add Side-By-Side settings and support
    This commit introduces `git_gutter_open_file` command as wrapper for the
    legacy `open_file` command, which is used for backwards compatibility.
    
    Beginning with Sublime Text 3124 the `open_file` commands are hidden as they
    are replaced by a more convenient `edit_settings` (Side-By-Side settings).
    
    All available settings are added to the command pallet as
    - Preferences: GitGutter Settings
    - Preferences: GitGutter Key Bindings
    - Preferences: GitGutter Popup Stylesheet
    
    The Main.sublime-menu was updated
    - to show Side-By-Side settings
    - to order JSON keys the same way for all items.
    - use ${platform} as part of the "file" argument to shorten the file
      which is possible as we do only use platform depended key bindings.

--- git_gutter_settings.py
+++ git_gutter_settings.py
@@ -1,12 +1,54 @@
 import os
 import shutil
 import sublime
+from sublime_plugin import ApplicationCommand
 
-ST3 = int(sublime.version()) >= 3000
+STVER = int(sublime.version())
+ST3 = STVER >= 3000
 
 
 def plugin_loaded():
     settings.load_settings()
+
+
+class GitGutterOpenFileCommand(ApplicationCommand):
+    """This is a wrapper class for SublimeText's `open_file` command.
+
+    The task is to hide the command in menu if `edit_settings` is available.
+    """
+
+    def run(self, file):
+        """Expand variables and open the resulting file.
+
+        NOTE: For some unknown reason the `open_file` command doesn't expand
+              ${platform} when called by `run_command`, so it is expanded here.
+        """
+        platform_name = {
+            'osx': 'OSX',
+            'windows': 'Windows',
+            'linux': 'Linux',
+        }[sublime.platform()]
+        file = file.replace('${platform}', platform_name)
+        sublime.run_command('open_file', {'file': file})
+
+    def is_visible(self):
+        """Return True to to show the command in command pallet and menu."""
+        return STVER < 3124
+
+
+class GitGutterEditSettingsCommand(ApplicationCommand):
+    """This is a wrapper class for SublimeText's `open_file` command.
+
+    Hides the command in menu if `edit_settings` is not available.
+    """
+
+    def run(self, **kwargs):
+        """Expand variables and open the resulting file."""
+        sublime.run_command('edit_settings', kwargs)
+
+    def is_visible(self):
+        """Return True to to show the command in command pallet and menu."""
+        return STVER >= 3124
 
 
 class GitGutterSettings:
