jisaacks/GitGutter

commit 4a95320f5c477f860c0b9541c2b2d5b33a60d022

Fix: All export-ignored files are marked as new files (Issue #409)
Issue:

Users repetitively report files being marked as new even though they aren't.

Since commit dce471e GitGutter uses `git archive` to read the content of a file
from the tree. This change was made to fix issue #74 and add some basic support
for smudge/clean filters.

But `git archive` doesn't find files which match the `export-ignore` patterns
in the `.gitattributes` or `.git/info/attributes` files. This results in empty
output for a file which is expected to be there and thus will be displayed as
added in the gutter area.

Solution:

The plumping command `git cat-file --filters` reads the file content with
smudge filters applied as well. Thus it is used to replace `git archive` to
fix this issue without breaking smudge/clean filters support.

modules/handler.py
@@ -649,44 +649,22 @@ class GitGutterHandler(object):
     def git_read_file(self, commit):
         """Read the content of the file from specific commit.
 
-        This method uses `git archive` to read the file content from git index
-        to enable support of smudge filters (fixes Issue #74). Git applies
-        smudge filters to some commands like `archive`, `diff` and `checkout`
-        only, but not to commands like `show`.
+        This method uses `git cat-files` to read the file content from git
+        index to enable support of smudge filters (fixes Issue #74). Git
+        applies smudge filters to some commands like `archive`, `diff`,
+        `checkout` and `cat-file` only, but not to commands like `show`.
 
         Arguments:
             commit (string): The identifier of the commit to read file from.
 
         Returns:
-            Promise: A promise to read and unzip the content of a file from git
-                index which will be resolved with the inflated file content.
+            Promise: A promise to read the content of a file from git index.
         """
-        def unzip(output):
-            """Unzip file binary content from git output.
-
-            Arguments:
-                output (string): Binary output returned by git containing the
-                    zipped content of the read file or None on error.
-
-            Returns:
-                string: Unzipped file content or None if git didn't return
-                    zipped file content. Error is already printed in that case.
-            """
-            try:
-                # Extract file contents from zipped archive.
-                # The `filelist` contains numerous directories finalized
-                # by exactly one file whose content we are interested in.
-                archive = zipfile.ZipFile(BytesIO(output))
-                return archive.read(archive.filelist[-1])
-            except:
-                return None
-
         args = [
             self.settings.git_binary,
-            'archive', '--format=zip',
-            commit, self._git_path
+            'cat-file', '--filters', ':'.join((commit, self._git_path))
         ]
-        return self.execute_async(args=args, decode=False).then(unzip)
+        return self.execute_async(args=args, decode=False)
 
     def execute_async(self, args, decode=True):
         """Execute a git command asynchronously and return a Promise.

commit 1f05f5018da9fe3e357adf9dd276240de038d0f2

Fix: Use git --filters argument only if git 2.11.0+ is available (Issue #416)
Commit 4a95320 introduced `git cat-file --filters` to read content from a tree object and apply smudge filters. The reason for that change was `git archive` ignoring files which are filtered from export in a .gitattributes file.

With release of GitGutter 1.7.0 it turned out that `--filters` argument is supported by git 2.11.0 and above only, which may not be available on some conservative Linux distributions. As a result of the failed execution GitGutter sees an empty file and marks all lines added.

The only solution right now is to use `-p` instead of `--filters` if older git versions are detected. This will reintroduce issue #74 for all users of legacy git setups but there is no other solution available.

Note: smudge filters require git 2.11.0+ beginning with this commit.

modules/handler.py
@@ -108,7 +108,7 @@ class GitGutterHandler(object):
             match = re.match(r'git version (\d+)\.(\d+)\.(\d+)', git_version)
             if match:
                 # PEP-440 conform git version (major, minor, patch)
-                self._git_version = (int(group) for group in match.groups())
+                self._git_version = tuple(int(g) for g in match.groups())
                 if git_binary in self._missing_binaries:
                     utils.log_message(git_binary + ' is back on duty!')
                     self._missing_binaries.discard(git_binary)
@@ -702,7 +702,10 @@ class GitGutterHandler(object):
         """
         args = [
             self.settings.git_binary,
-            'cat-file', '--filters', ':'.join((commit, self._git_path))
+            'cat-file',
+            # smudge filters are supported with git 2.11.0+ only
+            '--filters' if self._git_version >= (2, 11, 0) else '-p',
+            ':'.join((commit, self._git_path))
         ]
         return self.execute_async(args=args, decode=False)
 