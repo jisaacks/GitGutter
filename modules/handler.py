jisaacks/GitGutter

commit 9e5ab88c1014ba0ed07e66b8e09178d52144548d

Fix: Reduce gutter icon jumping while editing text
As evaluation of changes and updating the gutter icons is handled asynchronously, the view might change in the meanwhile, rendering the evaluation result invalid or at least inaccurate. As the result lines can't be translated to correct regions anymore. Therefore added regions may jump up or down to the next line.

To reduce this effect two changes are made:

1. initial evaluation delay is increased to 200ms
2. updating regions (gutter icons) is skipped if the view change counter has changed during evaluation

modules/handler.py
@@ -243,6 +243,13 @@ class GitGutterHandler(object):
         """
         return self.git_tracked
 
+    def view_file_changed(self):
+        """Check whether the content of the view changed."""
+        return (
+            not _HAVE_VIEW_CHANGE_COUNT or 
+            self._view_change_count != self.view.change_count()
+        )
+
     def invalidate_view_file(self):
         """Reset change_count and force writing the view cache file.
 

modules/events.py
@@ -172,7 +172,7 @@ class ViewEventListener(object):
         self.events |= event_id
         if not self.busy:
             self.delay = max(200, self.settings.get('debounce_delay', 1000))
-            self.start_timer(100)
+            self.start_timer(200)
 
     def start_timer(self, delay):
         """Run GitGutterCommand after some idle time.

modules/show_diff.py
@@ -88,8 +88,9 @@ class GitGutterShowDiff(object):
             self._line_height = view.line_height()
             self._minimap_size = self.git_handler.settings.show_in_minimap
             regions = self._contents_to_regions(contents)
-            for name, region in zip(self.region_names, regions):
-                self._bind_regions(name, region)
+            if not self.git_handler.view_file_changed():
+                for name, region in zip(self.region_names, regions):
+                    self._bind_regions(name, region)
             self._update_status(
                 'modified' if contents[0] else 'committed', contents)
         except IndexError:

commit e194745e561e8719719bc36b973df8b2c9fa0b67

Fix: Gutter icons are not displayed in Sublime Text 2
The issue was introduced with commit 9e5ab88 which intends to reduce gutter icon jumping

The view change counter is used to check whether to update the regions or not. If this counter is not available the gutter icons are never updated as the related function returns true in that case.

This commit changes the behavior of the function to return False if the change counter is not available.

modules/handler.py
@@ -246,7 +246,7 @@ class GitGutterHandler(object):
     def view_file_changed(self):
         """Check whether the content of the view changed."""
         return (
-            not _HAVE_VIEW_CHANGE_COUNT or 
+            _HAVE_VIEW_CHANGE_COUNT and
             self._view_change_count != self.view.change_count()
         )
 