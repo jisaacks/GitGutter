jisaacks/GitGutter

commit 3ba34d4e09828a693481923a928ff52f63b14c1f

Internal: Refactor built and release system
GitGutter's built and release system relies on ruby, make and bash scripts.

1. They are not available by nature on every operating system.
2. Sublime Text's python engine can do the same thing without external dependencies.
3. The release message files exist twice in the repository.

The goals of this commit are to ...

1. Built & publish releases directly from ST3.
2. Store release messages in "messages" folder once as all the other packages do.
3. Keep the opportunity to make releases from the shell if desired.

---

With this commit, releasing a new version of GitGutter requires the following steps:

1. Set-Up the GitHub access token (needed once)

   - Call "Preferences: GitGutter Settings" from command pallet
   - Add a key "github_token": "..." to the user file.

2. Create a proper message file in the "messages" folder.

   - The name is the version being used to release.
   - The first line of the file contains the version as heading for ST.
   - The global message is not required (it is added by step 3)

3. Call "GitGutter: Build Release" from command pallet

   This will create a "Cut 1.x.x" commit and tag locally.

4. Call "GitGutter: Publish Release" from command pallet

   This will ...
   - push the latest commit of master branch
   - push the release tag
   - create a release with the release message from the most recent messages/1.x.x.txt

The "Release" commands are visible only, if the "github_token" is set up.

---

The release.py also provides a CLI to run it from shell as follows:

  ./release build
  ./release publish --token <thetoken>

The makefile was modified as well.

---

All release-related files are added to the .gitattributes to prevent them from being deployed in ordinary sublime-package files.

release.py
@@ -1,0 +1,231 @@
+#!/usr/bin/python
+
+import json
+import os
+import subprocess
+
+PACKAGE_PATH = os.path.dirname(__file__)
+MESSAGE_DIR = 'messages'
+MESSAGE_PATH = os.path.join(PACKAGE_PATH, MESSAGE_DIR)
+
+GLOBAL_MESSAGE = """
+
+
+💛 Git Gutter? Want to support development?
+
+I've teamed up with Wes Bos to offer the following discounts:
+
++------------------------------------------------+
+|                                                |
+|   Use the coupon code GITGUTTER for $10 off    |
+|                                                |
++------------------------------------------------+
+
+🏅 ⭐ ES6 ⭐
+
+👉 ES6.io/friend/GITGUTTER
+
+🏅 ⭐ Sublime Text Book ⭐
+
+👉 SublimeTextBook.com/friend/GITGUTTER
+
+🏅 ⭐ React For Beginners ⭐
+
+👉 ReactForBeginners.com/friend/GITGUTTER
+
+
+Join 15,000 other developers already learning with Wes Bos.
+
+These are fantastic resources - 100% money back guarantee! 🌟
+"""
+
+
+def get_message(fname):
+    with open(fname, 'r', encoding='utf-8') as file:
+        message = file.read()
+    return message
+
+
+def put_message(fname, text):
+    with open(fname, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+
+def add_global_message(fname):
+    """Append the GLOBAL_MESSAGE to a file if not yet contained."""
+    text = get_message(fname)
+    if GLOBAL_MESSAGE not in text:
+        put_message(fname, text.strip() + GLOBAL_MESSAGE)
+
+
+def remove_global_message(fname):
+    """Remove the GLOBAL_MESSAGE from a file."""
+    text = get_message(fname)
+    new_text = text.replace(GLOBAL_MESSAGE, '').strip() + '\n'
+    if new_text != text:
+        put_message(fname, new_text)
+
+
+def update_global_message(version_history):
+    # remove global message from previous release
+    remove_global_message(
+        os.path.join(MESSAGE_PATH, version_history[-3] + '.txt'))
+    # add global message to current release
+    add_global_message(
+        os.path.join(MESSAGE_PATH, version_history[-2] + '.txt'))
+
+
+def built_messages_json(version_history):
+    """Write the version history to the messages.json file."""
+    output = os.path.join(PACKAGE_PATH, 'messages.json')
+    with open(output, 'w+', encoding='utf-8') as file:
+        json.dump(
+            obj={v: MESSAGE_DIR + '/' + v + '.txt' for v in version_history},
+            fp=file, indent=4, separators=(',', ': '), sort_keys=True)
+        file.write('\n')
+
+
+def version_history():
+    """Return a list of all releases."""
+    def generator():
+        for filename in os.listdir(MESSAGE_PATH):
+            basename, ext = os.path.splitext(filename)
+            if ext.lower() == '.txt':
+                yield basename
+    return tuple(generator())
+
+
+def git(*args):
+    """Run git command within current package path."""
+    if os.name == 'nt':
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    else:
+        startupinfo = None
+    proc = subprocess.Popen(
+        args=['git'] + [arg for arg in args], startupinfo=startupinfo,
+        stdout=subprocess.PIPE, stdin=subprocess.PIPE, cwd=PACKAGE_PATH)
+    stdout, _ = proc.communicate()
+    return stdout.decode('utf-8').strip() if stdout else None
+
+
+def commit_release(version):
+    """Create a 'Cut <version>' commit and tag."""
+    commit_message = 'Cut %s' % version
+    git('add', '.')
+    git('commit', '-m', commit_message)
+    git('tag', '-a', '-m', commit_message, version)
+
+
+def build_release():
+    """Built the new release locally."""
+    history = version_history()
+    version = history[-2]
+    update_global_message(history)
+    built_messages_json(history)
+    commit_release(version)
+    print("Release %s created!" % version)
+
+
+def publish_release(token):
+    """Publish the new release."""
+    history = version_history()
+    version = history[-2]
+
+    repo_url = 'https://github.com/jisaacks/GitGutter.git'
+    # push master branch to server
+    git('push', repo_url, 'master')
+    # push tags to server
+    git('push', repo_url, 'tag', version)
+
+    # publish the release
+    post_url = '/repos/jisaacks/GitGutter/releases?access_token=' + token
+    headers = {
+        'User-Agent': 'Sublime Text',
+        'Content-type': 'application/json',
+    }
+    # get message from /messages/<version>.txt
+    text = get_message(os.path.join(MESSAGE_PATH, version + '.txt'))
+    # strip global message
+    text = text.replace(GLOBAL_MESSAGE, '').strip()
+    # strip message header (version)
+    text = text[text.find('\n') + 1:]
+    # built the JSON request body
+    data = json.dumps({
+        "tag_name": version,
+        "target_commitish": "master",
+        "name": version,
+        "body": text,
+        "draft": False,
+        "prerelease": False
+    })
+    try:
+        import http.client
+        client = http.client.HTTPSConnection('api.github.com')
+        client.request('POST', post_url, body=data, headers=headers)
+        response = client.getresponse()
+        print("Release %s published!" % version
+              if response.status == 201 else
+              "Release %s failed!" % version)
+    finally:
+        client.close()
+
+
+"""
+======================================
+Command Line Interface
+======================================
+"""
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Built and Publish GitGutter Releases')
+    parser.add_argument(
+        dest='command',
+        help='The command to perform is one of [BUILD|PUBLISH].')
+    parser.add_argument(
+        '--token',
+        nargs='?',
+        help='The GitHub access token used for authentication.')
+    args = parser.parse_args()
+    if args.command.lower() == 'build':
+        build_release()
+    elif args.command.lower() == 'publish':
+        publish_release(args.token)
+
+
+"""
+======================================
+Sublime Text Command Interface
+======================================
+"""
+try:
+    import sublime
+    import sublime_plugin
+
+    SETTINGS = "GitGutter.sublime-settings"
+
+    class GitGutterBuildReleaseCommand(sublime_plugin.ApplicationCommand):
+
+        def is_visible(self):
+            settings = sublime.load_settings(SETTINGS)
+            return settings.has('github_token')
+
+        def run(self):
+            """Built a new release."""
+            build_release()
+
+    class GitGutterPublishReleaseCommand(sublime_plugin.ApplicationCommand):
+
+        def is_visible(self):
+            settings = sublime.load_settings(SETTINGS)
+            return settings.has('github_token')
+
+        def run(self):
+            """Publish the new release."""
+            settings = sublime.load_settings(SETTINGS)
+            publish_release(settings.get('github_token', ''))
+
+except ImportError:
+    pass

release_messages/dest/VERSION
@@ -1,1 +1,0 @@
-1.7.4

.gitattributes
@@ -5,9 +5,10 @@ gh-pages/ export-ignore
 *.gitignore export-ignore
 *.gitattributes export-ignore
 
-## release message sources
-release_messages/src/ export-ignore
+## release scripts
 makefile export-ignore
+release.py export-ignore
+release.sublime-commands export-ignore
 
 ## unit testing
 tests/ export-ignore

makefile
@@ -1,5 +1,6 @@
 build_release:
-	./release_messages/src/build.rb
+	./release build
 
-release: build_release
-	./release_messages/src/release.sh
+release:
+	build_release
+	./release publish --token `cat $HOME/.github_access_token`

release.sublime-commands
@@ -1,0 +1,10 @@
+[
+    {
+        "caption": "GitGutter: Build Release",
+        "command": "git_gutter_build_release"
+    },
+    {
+        "caption": "GitGutter: Publish Release",
+        "command": "git_gutter_publish_release"
+    }
+]

release_messages/dest/PRE_RELEASE
@@ -1,1 +1,0 @@
-false

release_messages/src/build.rb
@@ -1,52 +1,0 @@
-#!/usr/bin/ruby
-
-require "fileutils"
-require "json"
-
-json = {}
-
-# Get messages in src in the correct order
-files = Dir.glob(File.join(%w{release_messages src messages *.txt})).sort_by do |file|
-  name = File.basename(file, '.txt')
-  if name == 'install'
-    [0, 0, 0, 0]
-  else
-    if name =~ /^(.*)-(?:pre|rc)(.*)/
-      name = $1.split('.').map(&:to_i).push(0).push($2)
-    else
-      name.split('.').map(&:to_i).push(1)
-    end
-  end
-end
-
-# Grab the global message
-global_message = File.read(File.join(%w{release_messages src global_message.txt}))
-
-# Remove previous build
-FileUtils.rm_rf(File.join(%w{release_messages dest .}), secure: true)
-puts "\033[0;31mDeleted\033[0m #{File.join(%w{release_messages dest *})}"
-
-# Write the files
-files.each do |file|
-  contents = File.read(file)
-  path = File.join(%w{release_messages dest}.push(File.basename(file)))
-  name = File.basename(path, ".txt")
-  json[name] = path
-  msg = "\033[0;32mCreated\033[0m #{path}"
-  if file == files.last || file == files.first
-    msg += "\033[1;35m (with global message)\033[0m"
-    contents += "\n\n" unless contents.empty?
-    contents += global_message
-    if file == files.last
-      File.write(File.join(%w{release_messages dest VERSION}), name)
-      File.write(File.join(%w{release_messages dest PRE_RELEASE}), !!(name =~ /(?:pre|rc)/))
-    end
-  end
-  File.write(path, contents)
-  puts msg
-end
-
-File.write("messages.json", JSON.pretty_generate(json))
-puts "\033[0;32mCreated\033[0m messages.json"
-
-puts "\033[1;32mDone.\033[0m"

release_messages/src/release.sh
@@ -1,9 +1,0 @@
-TOKEN="`cat $HOME/.github_access_token`"
-VERSION="`cat ./release_messages/dest/VERSION`"
-PRE_RELEASE="`cat ./release_messages/dest/PRE_RELEASE`"
-SHA="`git rev-parse HEAD`"
-API_JSON=$(printf '{"tag_name": "%s","prerelease": %s,"target_commitish": "%s"}' "$VERSION" "$PRE_RELEASE" "$SHA")
-API_ENDPOINT=$(printf 'https://api.github.com/repos/jisaacks/GitGutter/releases?access_token=%s' "$TOKEN")
-curl --data "$API_JSON" "$API_ENDPOINT"
-echo "\033[0;32mCreated\033[0m release \033[1;33m$VERSION\033[0m"
-echo "\033[1;32mDone.\033[0m"

commit e2077c6e006acfd2f060aef03c4ef8f89c4db362

Fix: Support Info module provides no GitGutter version.
The issue was introduced with commit 3ba34d4.

The VERSION file no longer existed and could no longer be read by the support module.

This commit adds the VERSION which is generated during release to the root of the package.
The 'messages' folder must only contain the install.txt and the release messages. Otherwise the current release process would fail.

VERSION
@@ -1,0 +1,1 @@
+1.7.5

modules/support.py
@@ -55,7 +55,7 @@ def gitgutter_version():
     except:
         try:
             return sublime.load_resource(
-                'Packages/%s/release_messages/dest/VERSION' % PACKAGE)
+                'Packages/%s/VERSION' % PACKAGE).strip()
         except Exception as exception:
             print('%s: %s' % (PACKAGE, exception))
             return 'Version could not be acquired!'

release.py
@@ -121,6 +121,7 @@ def build_release():
     """Built the new release locally."""
     history = version_history()
     version = history[-2]
+    put_message(os.path.join(PACKAGE_PATH, 'VERSION'), version)
     update_global_message(history)
     built_messages_json(history)
     commit_release(version)