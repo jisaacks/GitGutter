jisaacks/GitGutter

commit 58139101bdfc47dd507a6ab33a433b6872216c9d

Fix: Disable line annotations in distraction free mode (#514)
Fixes #514

Sublime Text 3176 does not place phantoms correctly, if `draw_centered`
setting is set `true`.

The core issue is reported at:

  https://github.com/SublimeTextIssues/Core/issues/2416

modules/annotation.py
@@ -69,10 +69,14 @@ class GitGutterLineAnnotation(object):
     def is_enabled(self):
         """Check if blame phantom text is enabled.
 
+        Note:
+            Keep disabled if `draw_centered` is True because ST3176 does not
+            place the phantom to the correct place then.
+
         Returns:
             bool: True if blame phantom text is enabled, False otherwise.
         """
-        if not _HAVE_PHANTOMS:
+        if not _HAVE_PHANTOMS or self.settings.get('draw_centered'):
             return False
         show_phantoms = self.settings.get('show_line_annotation', 'auto')
         if show_phantoms == 'auto':

commit 2e7f7639d5c50d69e5742e883081a51efcdac73c

Fix: Disable line annotations if content is centered (#514)
Fixes #514

Fix for an regression in 58139101bdfc47dd507a6ab33a433b6872216c9d

The wrong settings() object was queried for the `draw_centered` setting. Therefore the fix did not actually work.

modules/annotation.py
@@ -76,7 +76,7 @@ class GitGutterLineAnnotation(object):
         Returns:
             bool: True if blame phantom text is enabled, False otherwise.
         """
-        if not _HAVE_PHANTOMS or self.settings.get('draw_centered'):
+        if not _HAVE_PHANTOMS or self.view.settings().get('draw_centered'):
             return False
         show_phantoms = self.settings.get('show_line_annotation', 'auto')
         if show_phantoms == 'auto':