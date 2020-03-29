ea1b9363401db67ab546e4551e906271f6267fba
DeathAxe <deathaxe82@googlemail.com>
2020-03-29 14:53:48 +0200

    Internal: Garbage collect unused global

--- plugin.py
+++ plugin.py
@@ -16,5 +16,6 @@ else:
             module_name for module_name in sys.modules
             if module_name.startswith(prefix) and module_name != __name__]:
         del sys.modules[module_name]
+    prefix = None
 
     from .modules import *
